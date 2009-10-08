210b7ff03de760b2c2c303aba4f47333c5f87b53
scottc <scott@scottcampbell.com.au>
2009-10-08 05:36:27 +0000

    Added some vector functions.

--- pong/geom/Vector.hx
+++ pong/geom/Vector.hx
@@ -9,10 +9,31 @@ class Vector
 {
 	public var x:Float;
 	public var y:Float;
-	public function new(x_:Float,y_:Float) 
+	public function new(x_:Float, y_:Float) 
 	{
 		x = x_; 
 		y = y_;
 	}
-	
+	public inline function clone() {
+		return new Vector(x,y);
+	}
+	public inline function set(x_:Float, y_:Float) {
+		x = x_;
+		y = y_;
+	}
+	public inline function plus(v:Vector) {
+		return new Vector(x + v.x, y + v.y);
+	}
+	public inline function minus(v:Vector):Vector{
+		return new Vector(x - v.x, y - v.y);
+	}
+	public inline function dotProduct(v:Vector) {
+		return x * v.x + y * v.y;
+	}
+	public inline function crossProduct(v:Vector) {
+		return x * v.y - y * v.x;
+	}
+	public inline function length() {
+		return Math.sqrt(x * x + y * y);
+	}
 }
