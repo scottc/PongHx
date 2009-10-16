743973ee37ee834e2a7e34183902d86cc29fdc6a
scottc <scott@scottcampbell.com.au>
2009-10-16 13:11:20 +0000

    Added some more button code.

--- pong/gfx/Button.hx
+++ pong/gfx/Button.hx
@@ -2,15 +2,23 @@
 
 class Button extends Label
 {
-	public var function:Void;
+	public var object:Void;
+	public var function:String;
+	public var args:Array<Dynamic>;
 	
-	public function new(?text_:String = "",?function_:Void) 
+	public function new(?text_:String = "", object_:Void, function_:String, ?args_:Array<Dynamic>) 
 	{
 		super(text_);
+		object = object_;
 		function = function_;
+		args = args_;
+		
 		//backgroundColor = 0xff0000;
 		//borderThickness = 1;
 		//borderColor = 0x00ff00;
+	}
+	private function execFunction() {
+		Reflect.callMethod(object, function, args);
 	}
 	
 }
